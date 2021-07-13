2016cdef80512530899ebb62b11b82e4d3ad1b31
Joilson Miranda <joilsonmirandasl@hotmail.com>
2021-07-13 17:46:23 -0300

    chore: add Footer component

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,14 +1,27 @@
 <template>
     <footer>
-        
+        <a :href="socialLink.link" class="socialLink" v-for="(socialLink, key) in socialLinks" :key="key" target="_blank">
+            <img :src="socialLink.icon" :title="socialLink.label" :alt="socialLink.label">
+        </a>
     </footer>
 </template>
 
 <script>
-
+import api from '@/services/api.js';
 
 import './style.css';
 export default {
     name: 'Footer',
+    data() {
+        return {
+            socialLinks: []
+        }
+    },
+    mounted() {
+        // fazendo requisição na api via axios
+        api.get('/social-links.json').then(response => {
+            this.socialLinks = response.data;
+        });
+    }
 }
 </script>
